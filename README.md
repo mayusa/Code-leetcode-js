0a223238a71ea1e84b0a92378e7283e339a5a142
May <ashucn@gmail.com>
2015-05-23 13:07:53 -0700

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+
+##Step 1. install 3 dependences
+````
+npm install --save-dev mocha
+
+npm install --save-dev should
+
+npm install --save-dev supertest
+
+````
---
+++ code/1-twoSum.js
@@ -0,0 +1,27 @@
+
+/* 1. two sume
+Given an array of integers, find two numbers such that they add up to a specific target number.
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+You may assume that each input would have exactly one solution. Input: numbers={2, 7, 11, 15}, target=9
+Output: index1=1, index2=2
+*/
+
+var twoSum = function(a, n) {
+  a.sort(function(a, b) {
+    return a - b;
+  });
+  var dict = a.reduce(function(acc, e, i) {
+    acc[e] = i;
+    return acc;
+  }, {});
+  var ret = [];
+  a.forEach(function(e, i) {
+    var other = n-e;
+    if (dict[other]) {
+      ret = [i, dict[other]];
+    }
+  });
+  return ret;
+}
+
+module.exports = twoSum;
